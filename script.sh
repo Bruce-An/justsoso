3313c03b0628e7e6b36c446b9d6a5273a24fdf44
Bruce-An <74276458+Bruce-An@users.noreply.github.com>
2021-09-18 15:40:33 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,5 +18,5 @@ sid="${udata#*&sid=}" #登录获取ptopid和sid
 curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
 curl -d "@myvs.txt" -d "jingdu=113.39&weidu=34.47&ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
 udata=$(sed -n '24,26p' temp.txt)
-echo "$udata" +> result.html
+echo "$udata" >> result.html
 done
